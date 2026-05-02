f5e0782ed706c144a3392a48568f072462eeb1da
geomfazio7-tech <geom.fazio7@gmail.com>
2026-05-02 10:29:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,39 @@
-const CACHE_NAME = "cantiericloud-v1";
+const CACHE_NAME = "cantiericloud-v2";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.pathname.endsWith("/") || requestUrl.pathname.endsWith("/index.html")) {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
